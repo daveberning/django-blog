846324203720064d1f55c853ec89be993da75013
Dave Berning <daveberning@outlook.com>
2022-12-06 22:38:22 -0500

    completed assignment 8

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,34 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
+from django.http import Http404
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+    def get(self, request, *args, **kwargs):
+        published = Post.objects.exclude(published_date__exact=None)
+        try:
+            post = published.get(pk=self.kwargs['post_id'])
+        except Post.DoesNotExist:
+            raise Http404
+        return render(request, 'blogging/detail.html', {'post': post})
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type='text/html')
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
 
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+        return render(request, 'blogging/list.html', {'posts': posts})
